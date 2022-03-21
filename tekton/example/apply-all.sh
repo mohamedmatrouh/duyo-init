91fef6e004d7f3b7819b745057232dd431c827ad
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2022-03-21 17:48:54 +0100

    add some tasks

---
+++ tekton/example/apply-all.sh
@@ -0,0 +1,6 @@
+kubectl apply -f volumes/pv-pvc.yaml
+kubectl apply -f tasks/check-npm-install.yaml
+kubectl apply -f tasks/ls-build-project.yaml
+kubectl apply -f tasks/npm-task.yaml
+kubectl apply -f pipelines/pipeline.yaml
+kubectl apply -f pipelinerun/pipelinerun.yaml
