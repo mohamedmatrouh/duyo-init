1998a139328b19221889f4be78a6fe9d6569db9e
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2022-03-23 16:59:48 +0100

    --

--- tekton/example/apply-all.sh
+++ tekton/example/apply-all.sh
@@ -1,6 +1,7 @@
 kubectl apply -f volumes/pv-pvc.yaml
 kubectl apply -f tasks/check-npm-install.yaml
 kubectl apply -f tasks/ls-build-project.yaml
+kubectl apply -f tasks/git-clone.yaml
 kubectl apply -f tasks/npm-task.yaml
 kubectl apply -f pipelines/pipeline.yaml
 kubectl apply -f pipelinerun/pipelinerun.yaml
